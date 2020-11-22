3dad43c2bd2bafd44e517bbd38cf30fcadd8929d
nediaputrii <50162029+NediaPutrii@users.noreply.github.com>
2020-11-23 01:43:43 +0700

    upload project

---
+++ souvenir_bkt/admin/inc/header.php
@@ -0,0 +1,13 @@
+<div class="sidebar-toggle-box" style="background-color:black">
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation" style="background-color:black"></div>
+              </div>
+             <a class="logo" style="background-color:black"><p><b>WEB</b><b style="font-size: 17px">GIS</b> - <b>S</b>ouvenir <b>ADMIN (11 181152012 Nedia Putri Ismala)</b></b></p></a>
+             <h4>
+            <div class="top-menu">
+              <ul class="nav pull-right top-menu" style="margin-top: 6px">
+                   <a href="act/logout.php" class="logo1"><img src="../image/logout.png">
+                   <span>Logout</span></a>
+              </ul>
+            </div>
+            </h4>
+            
