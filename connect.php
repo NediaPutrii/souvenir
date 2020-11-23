623e314af75ec4977a53aadaa798cecdf61bbba5
nediaputrii <50162029+NediaPutrii@users.noreply.github.com>
2020-11-23 22:50:52 +0700

    ubah db name

--- connect.php
+++ connect.php
@@ -2,6 +2,6 @@
 	$host = "webgis.mysql.database.azure.com";
 	$user = "mysql@webgis";
 	$pass = "#12345678Webgis";
-	$dbname = "sys";
+	$dbname = "souvenir";
 	$conn = mysqli_connect($host, $user, $pass, $dbname) or die("Gagal");
 ?>
