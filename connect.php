88ddc059e36b53337918339f4467b11289e59243
nediaputrii <50162029+NediaPutrii@users.noreply.github.com>
2020-12-03 11:28:16 +0700

    update connection with heroku

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
-	$host = "webgis.mysql.database.azure.com";
-	$user = "mysql@webgis";
-	$pass = "#12345678Webgis";
-	$dbname = "souvenir";
+	$host = "us-cdbr-east-02.cleardb.com";
+	$user = "bf32bc0a3e8ec0";
+	$pass = "24cf7a4a";
+	$dbname = "heroku_3aa639d54060f10";
 	$conn = mysqli_connect($host, $user, $pass, $dbname) or die("Gagal");
 ?>
